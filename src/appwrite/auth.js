926292213a34bfd8b05ef3e0fb83ff88310f7e34
syedhamzasiff <syedhamzasiff@gmail.com>
2024-04-09 15:02:29 +0500

    handle auth

---
+++ src/appwrite/auth.js
@@ -0,0 +1,57 @@
+import conf from '../conf.js';
+import {Client, Account, ID} from "appwrite";
+
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client);
+    }
+
+    async createAccount({email, password, name}) {
+        try {
+            const userAccount = await this.account.create(ID.unique, email, password, name);
+            if (userAccount) {
+                //we can also send a success method here OR we could redirect the user to login directly after signup -> we have implemented the second option
+                return this.login({email, password});
+            } else {
+                return userAccount;
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async login({email, password}) {
+        try {
+            return await this.account.createEmailPasswordSession(email, password)
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
+        } catch (error) {
+            throw error;
+        }
+        return error;
+    }
+
+    async logout(){
+        try {
+            return await this.account.deleteSessions();
+        } catch (error) {
+            throw error;
+        }
+    }
+}
+
+const authService = new AuthService();
+
+export default authService; 
